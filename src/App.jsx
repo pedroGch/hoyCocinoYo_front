af071e2e7b3b3040405c8d74410465b136aa6ea8
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-17 15:27:20 -0300

    Desarrollo de Navbar y HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import HomePage from './pages/HomePage'
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from './pages/RegisterPage'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "/login",
     element: <LoginPage />,
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const drawerWidth = 240;
+
+function DrawerAppBar(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                MUI
+            </Typography>
+            <Divider />
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    const theme = createTheme({
+        palette: {
+            mode: 'light',
+            primary: {
+                main: '#f6e4e3',
+            },
+        },
+
+    })
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <ThemeProvider theme={theme}>
+                <AppBar component="nav">
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            textAlign="center"
+                            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                        >
+                            Hoy, cocino yo!
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </ThemeProvider>
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+    );
+}
+
+DrawerAppBar.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
+
+export default DrawerAppBar;
