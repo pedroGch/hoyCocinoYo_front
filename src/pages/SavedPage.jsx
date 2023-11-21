e7aa0a2da8e9b2ef5e1dfb2b61e7404b06376e14
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-11-21 18:17:28 -0300

    SavedPage

---
+++ src/pages/SavedPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import HeroSaved from '../components/HeroSaved'
+import Footer from '../components/Footer'
+
+const SavedPage = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSaved />
+      <Footer />
+    </>
+  )
+}
+
+export default SavedPage
