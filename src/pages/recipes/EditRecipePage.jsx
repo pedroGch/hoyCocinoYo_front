825da2e7f57554f89fafb8213bd2b2ba86f7baf1
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-27 19:39:16 -0300

    fix en inputs varios en Editar

--- src/pages/recipes/EditRecipePage.jsx
+++ src/pages/recipes/EditRecipePage.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { Box, Container, Divider, Typography, Chip } from '@mui/material'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import EditForm from '../../components/EditForm'
